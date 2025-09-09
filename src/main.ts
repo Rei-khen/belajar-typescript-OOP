8fe004939ce0eedaf996c3922564fbdba91648ff
Rei-khen <aidilradiansyah112@gmail.com>
2025-09-09 11:22:42 +0800

    membuat class di ts

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+//membuat class
+
+//standar pemuatan class adalah dengan diawali dengan huruf besar
+class Pembeli {
+  //property dan method
+}
+
+class Binatang {
+  //property dan method
+}
+
+//membuat objek dari class
+
+const pembeli1: Pembeli = new Pembeli();
+const kucing: Binatang = new Binatang();
+console.log("berhasil");
